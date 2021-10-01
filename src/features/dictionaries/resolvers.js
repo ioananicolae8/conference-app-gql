d35f3cb6572fca82be407083ad78a0abe424f83d
ioananicolae8 <ioananicolae12312@gmail.com>
2021-10-01 17:05:48 +0300

    format

--- src/features/dictionaries/resolvers.js
+++ src/features/dictionaries/resolvers.js
@@ -1,6 +1,28 @@
-const {randomCharacters} = require("../../utils/functions");
 
 const dictionaryResolvers = {
-    
+    Query:
+    {
+        typeList: async (_parent, _args, { dataSources }, _info) => {
+            const typeList = await dataSources.dictionaryDb.getTypeList()
+            return typeList
+        },
+        categoryList: async (_p, _a, { dataSources }) => {
+            const categoryList = await dataSources.dictionaryDb.getCategoryList()
+            return categoryList
+        },
+        countryList: async (_p, _a, { dataSources }) => {
+            const countryList = await dataSources.dictionaryDb.getCountryList()
+            return countryList
+        },
+        countyList: async (_p, _a, { dataSources }) => {
+            const countyList = await dataSources.dictionaryDb.getCountyList()
+            return countyList
+        },
+        cityList: async (_p, _a, { dataSources }) => {
+            const cityList = await dataSources.dictionaryDb.getCityList()
+            return cityList
+        }
+    }
 }
+
 module.exports = dictionaryResolvers;
